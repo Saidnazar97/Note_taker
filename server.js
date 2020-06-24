60dc9b0d00aefa82a177b8cc2b34fa0e2accfee9
Saidnazar97 <lifedada98@gmail.com>
2020-06-24 07:17:01 -0700

    add files

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3300;
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static("./Develop/client"));
+
+const apiRoutes = require("./Dev/server/api-routes");
+app.use(apiRoutes);
+
+const clientRoutes = require("./Dev/server/client-routes");
+app.use(clientRoutes);
+
+app.listen(PORT, () => console.log(`listening at: http://localhost:${PORT}`));
